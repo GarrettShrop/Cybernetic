f507d801d16a27c519d7e567f269f0acce84b5b9
Virus <gshropshire@csumb.edu>
2022-07-28 00:42:10 -0700

    added in functionality to start and end conversations with different users logging there usernames to see if there has been a past chat to make for a more realistic feel

--- events/message.js
+++ events/message.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const OpenAI = require('openai-api');
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
-let speaker;
+let speaker = [];
+let finishers = ['bye','goodnight','goodbye','im out'];
 let prompt =`The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
 Human: Hello, who are you?
 AI: I am an AI created by OpenAI. How can I help you today?
@@ -12,8 +13,18 @@ module.exports = {
 	name: 'messageCreate',
 	execute(messageCreate) {
         if (messageCreate.author.bot) return;
-        if (messageCreate.content.includes('cybernetic')||messageCreate.content.includes('Cybernetic') || speaker == messageCreate.author){
-            speaker = messageCreate.author;
+        if (messageCreate.content.includes('cybernetic')||messageCreate.content.includes('Cybernetic') || speaker.includes(messageCreate.author)){
+            if(!speaker.includes(messageCreate.author)){
+                speaker.append(messageCreate.author);
+            }
+            if(finishers.includes(messageCreate.content.toLowerCase())) {
+                for(var i = 0; i < speaker.length;i++){
+                    if(speaker[i]== messageCreate.author){
+                        speaker.splice(i,1);
+                    }
+                }
+                return;
+            }
             messageCreate.content.replace('cybernetic','');
         prompt += `You: ${messageCreate.content}\n`;
         (async () => {
