e9822aa930097b080bf2598251cc2c94fea7e245
Garrettshrop <typicalgamer456@gmail.com>
2023-07-04 02:35:12 +0100

    cleaned up index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-/* eslint-disable no-multiple-empty-lines */
-/* eslint-disable no-unused-vars */
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
