3ccd9b683fc9d7f2902a67754f6731a6b02827be
Garrettshrop <typicalgamer456@gmail.com>
2023-07-05 03:20:44 +0100

    added heroku db support

--- database/database.js
+++ database/database.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/cyberneticdb';
 
 const pool = new Pool({
-	user: process.env.DB_USERNAME,
-	host: process.env.DB_HOST,
-	database: process.env.DB_DATABASE,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
+	connectionString,
+	ssl: process.env.DATABASE_URL ? true : false,
 });
 
-module.exports = {
-	query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
+
