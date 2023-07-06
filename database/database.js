e5ee6aefedc420f350d88dc92d47202e77e704c7
Garrettshrop <typicalgamer456@gmail.com>
2023-07-06 23:15:30 +0100

    changing database.js to only use heroku hosted postgres db from now on to keep data in sync at all times

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/cyberneticdb';
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
 	connectionString,
